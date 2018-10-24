b55e569998b14a6094b3d3b2bbaa88621272a558
aulorbe <aulorbe@gmail.com>
2018-10-24 17:21:11 -0400

    anon cc number function completed

--- string_functions.py
+++ string_functions.py
@@ -1,9 +1,9 @@
+import re
+
 def say_hello(name):
-    def say_hello(name):
     print(f"Hi my name is {name}")
 
 def replace_given_substring(str_to_replace, str_to_insert, string):
-    def replace_given_substring(str_to_replace, str_to_insert, string):
     return string.replace(str_to_replace,str_to_insert)
 
 def remove_duplicate_punctuation(string):
@@ -12,20 +12,23 @@ def remove_duplicate_punctuation(string):
         s = re.sub('[' + spec + ']+', spec, s)
     return s
 
-
 def validate_email_format(email):
-    def validate_email_format(email):
     regex = '\w+\@\w+.com'
     output = re.match(regex,email)
     if output:
         return True
     else:
         return False
+validate_email_format('aulorbegmail.com')
 
 
-def anonymize_credit_card_number(credit_card_number):
-    # should replace all characters except the last 4 with 'X'
-    # return the anonymized credit card number as a string
-    # the credit card may have characters that are not numbers (i.e. spaces and dashes, which we would want to keep)
-    # i.e. 1234-5678-90-1234 -> XXXX-XXXX-XX-1234
-    pass
+def anonymize_credit_card_number(cc):
+    all_x = ''
+
+    for c in cc:
+        if c.isdigit():
+            all_x += ('X')
+        else:
+            all_x += (c)
+
+    return ((all_x[:-4]) + cc[-4:])
